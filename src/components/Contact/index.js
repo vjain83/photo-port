e689f6695fda2683d23682bfc03f29a4e0bf1854
Varsha Jain <csvarshajain@gmail.com>
2022-01-10 00:04:44 -0500

    deployment done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,79 +1,59 @@
 import React, { useState } from 'react';
 import { validateEmail } from '../../utils/helpers';
 
-
-
-
 function ContactForm() {
-    const [errorMessage, setErrorMessage] = useState('');
-
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
 
-    function handleChange(e) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!errorMessage) {
+            setFormState({ [e.target.name]: e.target.value });
+            console.log('Form', formState);
+        }
+    };
+    const handleChange = (e) => {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
-
-            console.log(isValid);
-            // isValid conditional statement
             if (!isValid) {
                 setErrorMessage('Your email is invalid.');
             } else {
-
-                if (!e.target.value.length) {
-                    setErrorMessage(`${e.target.name} is required.`);
-                } else {
-                    setErrorMessage('');
-                }
-
+                setErrorMessage('');
             }
-            console.log('errorMessage', errorMessage);
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
         }
-        // isValid conditional statement
-
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-        // setFormState({ ...formState, [e.target.name]: e.target.value })
-
-    }
-    console.log(formState);
-
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
-
+    };
     return (
-
         <section>
-            <h1>Contact me</h1>
-            <form id="contact-form" onClick={handleChange}>
+            <h1 data-testid="h1tag">Contact me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" onBlur={handleChange} name="name" defaultValue={name} />
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" onBlur={handleChange} defaultValue={email} />
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" onBlur={handleChange} defaultValue={message} />
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
                 </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button data-testid="button" type="submit">Submit</button>
             </form>
-
-            {errorMessage && (
-                <div>
-                    <p className="error-text">{errorMessage}</p>
-                </div>
-            )}
-
-            <button type="submit">Submit</button>
         </section>
-    )
+    );
 }
 
 export default ContactForm;
